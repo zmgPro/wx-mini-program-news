3beaeeaf390f6280624a57e320d71e5317aa251d
zmgPro <1067532194@qq.com>
2018-12-28 12:18:27 +0800

    add newslist request

--- pages/index/index.js
+++ pages/index/index.js
@@ -5,62 +5,54 @@ Page({
    * 页面的初始数据
    */
   data: {
-    title:["国内","国际","财经","娱乐","军事","体育","其他"]
+    title:["国内","国际","财经","娱乐","军事","体育","其他"],
+    newsList:[],
+
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
-
+  onLoad:function(){
+    this.getNewsList()
+    
   },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
 
   },
+  getNewsList(){
+    wx.request({
+      url: 'https://test-miniprogram.com/api/news/list',
+      data:{
+        type:'gn'
+      },
+      header:{
+        'content-type':'application/json'
+      },
+      success: (res) => {
+        let newsList=[]
+        let result=res.data.result
+        for (let i =0 ;i<9;i++){
+          newsList.push({
+            title: result[i].title,
+            date: result[i].date,
+            source: result[i].source,
+            image: result[i].firstImage
+          })
+        }
+        this.setData({
+          newsList:newsList
+        })
+        
+      },
+      fail: res => {
+        console.log(res)
+      }
 
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
+    })
 
   }
 })
